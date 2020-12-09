7c6f8e7f607970ad4dfd18fc5627535c8a7bc9b1
pfrodedelaforet <pfrode@DESKTOP-9M55EOO>
2020-12-09 15:16:22 +0100

    aaaa

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
 import pandas as pd 
+import numpy as np
+import scipy.stats
 def load_csv(file, labels):
     return pd.read_csv(file)[labels]
 
+def coeff_correl(col1, col2):
+    return np.corrcoef(col1, col2)
